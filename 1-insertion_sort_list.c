a6eb7576bcba54694ba9fbb0e1ae12628b811353
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-30 17:51:08 -0500

    Task 1 - Fixed pointer to head lost

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,52 +6,10 @@
 **/
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *next, *myList = *list;
-
-	current = *list;
-
-	next = (*current).next;
-
-	while (next != NULL)
-	{
-		while ((*current).n > (*next).n)
-		{
-			/*Separar nodo*/
-			(*current).next = (*next).next;
-			if ((*next).next != NULL)
-			{
-				(*(*current).next).prev = current;
-			}
-			(*next).next = NULL;
-			(*next).prev = NULL;
-			if ((*current).prev == NULL)
-			{
-				(*next).prev = (*current).prev;
-				(*next).next = current;
-				(*current).prev = next;
-				current = next;
-				next = (*current).next;
-			}
-			else
-			{
-				current = (*current).prev;
-				(*(*current).next).prev = next;
-				(*next).next = (*current).next;
-				(*current).next = next;
-				(*next).prev = current;
-			}
-			print_list(myList);
-		}
-		current = (*current).next;
-		next = (*current).next;
-	}
-}
-
-void insertion_sort_list(listint_t **list)
-{
 	listint_t *current, *next;
 
 	current = *list;
+
 	next = (*current).next;
 
 	while (next != NULL)
@@ -71,6 +29,7 @@ void insertion_sort_list(listint_t **list)
 				(*next).prev = (*current).prev;
 				(*next).next = current;
 				(*current).prev = next;
+				*list = next;
 				current = next;
 				next = (*current).next;
 			}
@@ -82,6 +41,7 @@ void insertion_sort_list(listint_t **list)
 				(*current).next = next;
 				(*next).prev = current;
 			}
+			print_list(*list);
 		}
 		current = (*current).next;
 		next = (*current).next;
