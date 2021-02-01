9f7045dff8deec251b6edb428c4a94aad2e6b01c
Wiston Venera Macías <wisvem@hotmail.com>
2021-02-01 16:10:03 -0500

    Task 4

---
+++ 100-shell_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+
+/**
+* shell_sort - shell sort algorithm
+* @array: array to sort
+* @size: array size
+**/
+void shell_sort(int *array, size_t size)
+{
+	size_t h = 1, i, n;
+	int number;
+
+	while (h < size / 3)
+	{
+		h = 3 * h + 1;
+	}
+	while (h > 0)
+	{
+		for (i = h; i < size; i += h)
+		{
+			for (n = i; n > 0 && array[n] < array[n - h]; n -= h)
+			{
+				number = array[n];
+				array[n] = array[n - h];
+				array[n - h] = number;
+			}
+		}
+		h = (h - 1) / 3;
+		print_array(array, size);
+	}
+}
