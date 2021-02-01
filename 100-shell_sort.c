edaea63a0b16e35ccf44d00a111698d3f49a8317
Wiston Venera Macías <wisvem@hotmail.com>
2021-02-01 18:30:48 -0500

    Task 4 fixed print

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,16 +8,17 @@
 void shell_sort(int *array, size_t size)
 {
 	size_t h = 1, i, n;
-	int number;
+	int number, sw;
 
 	if (!size)
 		return;
-	while (h < size / 3)
+	while (h < size)
 	{
 		h = 3 * h + 1;
 	}
 	while (h > 0)
 	{
+		sw = 0;
 		for (i = h; i < size; i++)
 		{
 			for (n = i; n > 0 && array[n] < array[n - h]; n -= h)
@@ -25,9 +26,11 @@ void shell_sort(int *array, size_t size)
 				number = array[n];
 				array[n] = array[n - h];
 				array[n - h] = number;
+				sw = 1;
 			}
 		}
 		h = h / 3;
-		print_array(array, size);
+		if (sw == 1)
+			print_array(array, size);
 	}
 }
