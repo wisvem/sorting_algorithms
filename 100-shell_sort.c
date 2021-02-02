06364b7ba8abacd471dbb932aed46d8e42a18ee8
Wiston Venera Macías <wisvem@hotmail.com>
2021-02-01 21:38:09 -0500

    Task 4 added condition to print

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,26 +8,28 @@
 void shell_sort(int *array, size_t size)
 {
 	size_t h = 1, i, n;
-	int number;
+	int number, sw = 0;
 
 	if (!size || !array)
 		return;
+	/*Get gap interval*/
 	while (h < size / 3)
-	{
 		h = 3 * h + 1;
-	}
 	while (h > 0)
 	{
+		sw = 1;
 		for (i = h; i < size; i++)
 		{
 			number = array[i];
 			for (n = i; n > h - 1 && array[n - h] > number; n = n - h)
 			{
 				array[n] = array[n - h];
+				sw = 0;
 			}
 			array[n] = number;
 		}
 		h = (h - 1) / 3;
-		print_array(array, size);
+		if (sw == 0)
+			print_array(array, size);
 	}
 }
