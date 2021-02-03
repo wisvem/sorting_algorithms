abac21c5b18394053e0daab90606e83a57212449
Wiston Venera Macías <wisvem@hotmail.com>
2021-02-03 18:53:44 -0500

    Cocktail counting

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,94 @@
+#include "sort.h"
+
+/**
+* cocktail_sort_list - elevator sort
+* @list: list
+**/
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *current = NULL;
+	int swapped = 1;
+
+	if (list)
+	{
+		current = *list;
+		while (swapped)
+		{
+			swapped = 0;
+			while (current->next)
+			{
+				if (current->n > current->next->n)
+				{
+					swapped = swp_fwd(current, list);
+					continue;
+				}
+				current = current->next;
+			}
+			if (!swapped)
+				break;
+			swapped = 0;
+			while (current->prev)
+			{
+				if (current->n < current->prev->n)
+				{
+					swapped = swp_bck(current, list);
+					continue;
+				}
+				current = current->prev;
+			}
+		}
+	}
+}
+
+/**
+* swp_fwd - Swaps forward two nodes of a linked list.
+*  @current: - Pointer to the current node to swap.
+*  @list: - Popinter to the list of nodes.
+*  Return:  1, Always.
+**/
+void swp_fwd(listint_t *current, listint_t **list)
+{
+	listint_t *moving = NULL;
+
+	moving = current->next;
+
+	moving->prev = current->prev;
+	if (current->prev)
+		current->prev->next = moving;
+	else
+		*list = current->next;
+
+	current->next = moving->next;
+	current->prev = moving;
+	moving->next = current;
+	if (current->next)
+		current->next->prev = current;
+
+	print_list(*list);
+	return (1);
+}
+/**
+* swp_bck - Swaps backward two nodes of a linked list.
+*  @current: - Pointer to the current node to swap.
+*  @list: - Popinter to the list of nodes.
+*  Return: 1, Always.
+**/
+int swp_bck(listint_t *current, listint_t **list)
+{
+	listint_t *moving = NULL;
+
+	moving = current->prev;
+
+	moving->next = current->next;
+	current->next->prev = moving;
+
+	current->prev = moving->prev;
+	current->next = moving;
+	moving->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	print_list(*list);
+	return (1);
+}
---
+++ 102-counting_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+* counting_sort - counting sort algorithm
+* @array: array with numbers to sort
+* @size: size of the array
+**/
+void counting_sort(int *array, size_t size)
+{
+	int max, position, *count = NULL, *order = NULL;
+	size_t i;
+
+	if (size == 0 || array == NULL)
+		return;
+	/*Find the maximun value in the array*/
+	max = array[0];
+	for (i = 0; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+	/*Use malloc(size of the max value) to create a new array for the count*/
+	count = malloc(sizeof(int) * (max + 1));
+	for (i = 0; i <= (size_t)max; i++)
+		count[i] = 0;
+	/*Save the counter values of every number in the count array*/
+	for (i = 0; i < size; i++)
+		count[array[i]] = count[array[i]] + 1;
+	/*Sum values in the count array*/
+	for (i = 1; i <= (size_t)max; i++)
+		count[i] = count[i] + count[i - 1];
+	print_array(count, (size_t)max + 1);
+	/*use malloc(same size of array) to create array to sort the numbers*/
+	order = malloc(sizeof(int) * size);
+	for (i = 0; i < size; i++)
+	{
+		count[array[i]] = count[array[i]] - 1;
+		position = count[array[i]];
+		order[position] = array[i];
+	}
+	/*Order the array by the order array*/
+	for (i = 0; i < size; i++)
+		array[i] = order[i];
+	free(count);
+	free(order);
+}
